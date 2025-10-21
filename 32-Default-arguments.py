473480116997e2f9685e1b595cf35596f2f2555e
Soldierkp1 <alejoxm9921@hotmail.com>
2025-10-20 23:17:02 -0400

    finished default arguments

--- 32-Default-arguments.py
+++ 32-Default-arguments.py
@@ -5,7 +5,17 @@
 #1. Positional, 2. Default, 3. Keyword, 4. Arbitrary
 
 
-def net_price(list_price, discount=0, tax=0.05):
-    return list_price * (1-discount) * (1+tax)
+# def net_price(list_price, discount=0, tax=0.05):
+#     return list_price * (1-discount) * (1+tax)
 
-print(net_price(500, 0.1, 0))
+# print(net_price(500, 0.1, 0))
+import time
+
+def count(end, start=0,):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+        
+    print("DONE!")
+    
+count(start=100, end=1000)
