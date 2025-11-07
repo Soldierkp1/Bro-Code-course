92a377c12233ae29a62b26d2942b6de90e0920ec
Soldierkp1 <alejoxm9921@hotmail.com>
2025-11-06 23:22:08 -0500

    some small studying, mostly watching the videos instead of typing them.

---
+++ 35-Iterables.py
@@ -0,0 +1,8 @@
+#Iterable is an object/collection that can return its elements one at a time, allowing it to be iterated over in a loop.
+
+num = [1, 2, 3, 4, 5]
+
+for item in num:
+    print(item, end="")
+
+#way to ez task, will skip.
---
+++ 36-Membership-operators.py
@@ -0,0 +1,4 @@
+#Membership operators are used to test whether a value or variable is found in a sequence.
+#in or not in.
+
+#also way too ez, will be skipping
